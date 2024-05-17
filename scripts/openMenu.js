d63ff055bf67ca690582656ed8f7f02881acb7f4
Kevin Daniel Morales Estrella <kdmorale@pichincha.com>
2024-05-17 11:14:47 -0500

    Week3 complete

--- scripts/openMenu.js
+++ scripts/openMenu.js
@@ -1,8 +1,9 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
-
+const box = document.querySelector('.herolabel');
 hamButton.addEventListener('click', () => {
 	console.log('open!');
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
+	box.classList.toggle('open');
 });
